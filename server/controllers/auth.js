abbf49480226244a0b641aa36c2e049775a22bb5
escaton <eho31@yandex.ru>
2014-06-12 20:30:14 +0400

    auto select callback

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,7 +8,7 @@ console.log(passportYandex.prototype);
 
 passportYandex.prototype.authorizationParams = function() {
     return {
-        state: 'escaton'
+        state: process.env.USER
     };
 }
 
