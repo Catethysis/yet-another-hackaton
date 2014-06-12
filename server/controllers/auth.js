411cdcbc2323f7d5dbbffd290e63b89e315b3c77
escaton <eho31@yandex.ru>
2014-06-12 20:04:45 +0400

    fix auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,10 +4,19 @@ var db = require('../lib/db');
 
 var users = {};
 
+console.log(passportYandex.prototype);
+
+passportYandex.prototype.authorizationParams = function() {
+    return {
+        state: 'escaton'
+    };
+}
+
 passport.use(new passportYandex({
         clientID: 'a55d6e3ea4d64578bb931fba21b84160',
         clientSecret: '49664a3c890d4d46b4bd5376375f5fde',
-        callbackURL: "http://escaton.twi.dev/auth/yandex/callback"
+        callbackURL: "http://callback.twi.dev/",
+        state: "escaton"
     },
     function(accessToken, refreshToken, profile, done) {
 
